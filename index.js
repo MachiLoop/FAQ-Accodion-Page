e22594ff56e560f57dbc838fcede42767e9f21c2
hamidu sodiq <hamidusodiq2@gmail.com>
2023-01-01 10:32:14 +0100

    worked on toggler icon and active state

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const quesHeading = document.querySelectorAll(".question-heading");
 const btn = document.querySelectorAll("button");
 const container = document.querySelector(".card-container");
 
+console.log(answers);
 console.log(quesHeading);
+console.log(btn);
 
 container.addEventListener("click", function (e) {
-  if (e.target.classList.contains("btn")) {
-    const index = e.target.dataset.index;
+  if (e.target.closest(".heading")) {
+    console.log(e.target.closest(".heading").querySelector(".btn"));
+    const index = e.target.closest(".heading").querySelector(".btn")
+      .dataset.index;
     // answers[index].classList.toggle("hidden");
     // quesHeading[index].classList.toggle("question-active");
 
@@ -15,11 +19,20 @@ container.addEventListener("click", function (e) {
       answers.forEach((ans) => ans.classList.add("hidden"));
       quesHeading.forEach((ques) => ques.classList.remove("question-active"));
       // answers[index].classList.add("hidden");
+      btn.forEach(
+        (btnEl) =>
+          (btnEl.style.backgroundImage = `url(images/icon-arrow-down.svg)`)
+      );
     } else {
       answers.forEach((ans) => ans.classList.add("hidden"));
       quesHeading.forEach((ques) => ques.classList.remove("question-active"));
+      btn.forEach(
+        (btnEl) =>
+          (btnEl.style.backgroundImage = `url(images/icon-arrow-down.svg)`)
+      );
       answers[index].classList.toggle("hidden");
       quesHeading[index].classList.toggle("question-active");
+      btn[index].style.backgroundImage = `url(images/icon-arrow-up.svg)`;
     }
   }
 });
