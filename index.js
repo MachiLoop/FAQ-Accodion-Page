09bf308869f2c3a1346db352b411e8098bb6172e
hamidu sodiq <hamidusodiq2@gmail.com>
2023-01-01 09:30:12 +0100

    Improved button toggler functionality

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ console.log(quesHeading);
 container.addEventListener("click", function (e) {
   if (e.target.classList.contains("btn")) {
     const index = e.target.dataset.index;
-    answers[index].classList.toggle("hidden");
-    quesHeading[index].classList.toggle("question-active");
+    // answers[index].classList.toggle("hidden");
+    // quesHeading[index].classList.toggle("question-active");
+
+    if (!answers[index].classList.contains("hidden")) {
+      answers.forEach((ans) => ans.classList.add("hidden"));
+      quesHeading.forEach((ques) => ques.classList.remove("question-active"));
+      // answers[index].classList.add("hidden");
+    } else {
+      answers.forEach((ans) => ans.classList.add("hidden"));
+      quesHeading.forEach((ques) => ques.classList.remove("question-active"));
+      answers[index].classList.toggle("hidden");
+      quesHeading[index].classList.toggle("question-active");
+    }
   }
 });
